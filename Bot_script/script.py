b55c740b85290a45ad1c13f27979c820f09e36b0
Abhishek <abhishekchaudhary0220@gmail.com>
2020-04-18 21:05:34 +0530

    reverting

--- Bot_script/script.py
+++ Bot_script/script.py
@@ -151,7 +151,7 @@ async def abuse(ctx, val):
 
 #API taken from Advice Slip JSON API website https://api.adviceslip.com/
 @bot.command()
-async def random(ctx):
+async def advice(ctx):
     URL = "https://api.adviceslip.com/advice" 
     r = requests.get(url = URL) 
     data = r.json() 
